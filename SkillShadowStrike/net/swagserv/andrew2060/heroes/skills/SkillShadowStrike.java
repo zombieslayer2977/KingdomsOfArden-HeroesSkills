81eadd8709db795426e48b436088494931c0ecd3
Andrew2060 <2060andrew@gmail.com>
2013-06-07 00:43:26 -0700

    Broadcast Execute Text on Use

--- SkillShadowStrike/net/swagserv/andrew2060/heroes/skills/SkillShadowStrike.java
+++ SkillShadowStrike/net/swagserv/andrew2060/heroes/skills/SkillShadowStrike.java
@@ -55,6 +55,7 @@ public class SkillShadowStrike extends TargettedSkill {
 		cT.addEffect(new ExpirableEffect(this, plugin, "ShadowStrikeSilence", 4000));
 		lE.addPotionEffect(PotionEffectType.SLOW.createEffect(60, 2));
 		Skill.damageEntity(lE, h.getEntity(), (int) (lE.getMaxHealth()*5*0.01), DamageCause.ENTITY_ATTACK);
+		broadcastExecuteText(h, lE);
 		return SkillResult.NORMAL;
 	}
 
