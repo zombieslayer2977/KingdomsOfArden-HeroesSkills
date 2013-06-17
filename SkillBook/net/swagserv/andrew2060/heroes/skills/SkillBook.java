1ae66064e6efcbe4dec5e8f34675d1b17479464f
Andrew2060 <2060andrew@gmail.com>
2013-06-16 20:50:45 -0700

    Type Checks

--- SkillBook/net/swagserv/andrew2060/heroes/skills/SkillBook.java
+++ SkillBook/net/swagserv/andrew2060/heroes/skills/SkillBook.java
@@ -48,34 +48,41 @@ public class SkillBook extends ActiveSkill {
 		if(!executors.containsKey(p)) {
 			ItemStack hand = p.getItemInHand();
 			if(!hand.getType().equals(Material.ENCHANTED_BOOK)) {
-				p.sendMessage(ChatColor.GRAY + "This is not an Enchanted Book!");
+				p.sendMessage(ChatColor.GRAY + "§7[§2Skill§7] This is not an Enchanted Book!");
 				return SkillResult.INVALID_TARGET_NO_MSG;
 			}
 			Map<Enchantment, Integer> enchant = ((EnchantmentStorageMeta)hand.getItemMeta()).getStoredEnchants();
 			executors.put(p, new PlayerExecuteData(enchant, System.currentTimeMillis() + 30000 , hand, p.getInventory().getHeldItemSlot()));
-			p.sendMessage(ChatColor.GRAY + "Select an item to enchant by using this skill again!");
+			p.sendMessage(ChatColor.GRAY + "§7[§2Skill§7] Select an item to enchant by using this skill again!");
 			return SkillResult.INVALID_TARGET_NO_MSG; //Prevent cooldowns/reagent use from triggering
 		} else {
 			if(executors.get(p).expirationTime <= System.currentTimeMillis()) {
-				p.sendMessage(ChatColor.GRAY + "Your selection has expired, please try again");
+				p.sendMessage(ChatColor.GRAY + "§7[§2Skill§7] Your selection has expired, please try again");
 				executors.remove(p);
 				return SkillResult.INVALID_TARGET_NO_MSG;
 			}
 			ItemStack tool = p.getItemInHand();
 			if(!(Util.isArmor(tool.getType()) || Util.isWeapon(tool.getType()))) {
-				p.sendMessage(ChatColor.GRAY + "This is not an enchantable item!");
+				p.sendMessage(ChatColor.GRAY + "§7[§2Skill§7] This is not an enchantable item!");
 				return SkillResult.INVALID_TARGET_NO_MSG;
 			}
 			PlayerExecuteData struct = executors.get(p);
 			executors.remove(p);
 			if(p.getInventory().getItem(struct.heldSlot) == null || (!p.getInventory().getItem(struct.heldSlot).equals(struct.hand))) {
-				p.sendMessage(ChatColor.GRAY + "Cannot find the original enchantment book inside your inventory anymore! Did you move it?");
+				p.sendMessage(ChatColor.GRAY + "§7[§2Skill§7] Cannot find the original enchantment book inside your inventory anymore! Did you move it?");
 				return SkillResult.FAIL;
 			}
 			ItemStack item = p.getInventory().getItem(struct.heldSlot);
-			item.setType(Material.AIR);
-			p.updateInventory(); //Blah blah deprecated but bukkit doesn't include new functionality for it
-			tool.addEnchantments(struct.enchant);
+		
+			try {
+				tool.addEnchantments(struct.enchant);
+				p.sendMessage(ChatColor.GRAY + "§7[§2Skill§7] Enchantment Successfully applied from book!");
+				item.setType(Material.AIR);
+				p.updateInventory(); //Blah blah deprecated but bukkit doesn't include new functionality for it
+			} catch (IllegalArgumentException e) {
+				p.sendMessage(ChatColor.GRAY + "§7[§2Skill§7] Enchantment cannot be applied to this type of item!");
+				return SkillResult.FAIL;
+			}
 		}
 		return SkillResult.NORMAL;
 	}
