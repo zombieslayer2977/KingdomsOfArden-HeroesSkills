b26aab40db39b4e0e70ac35c0ceddff28a892eb4
Andrew2060 <2060andrew@gmail.com>
2013-07-25 23:31:21 -0700

    Initial Commit Enhanced Agility Skill

---
+++ SkillEnhancedAgility/net/kingdomsofarden/andrew2060/heroes/skills/SkillEnhancedAgility.java
@@ -0,0 +1,39 @@
+package net.kingdomsofarden.andrew2060.heroes.skills;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import net.kingdomsofarden.andrew2060.toolhandler.ToolHandlerPlugin;
+
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.skill.ActiveSkill;
+
+public class SkillEnhancedAgility extends ActiveSkill {
+
+    public SkillEnhancedAgility(Heroes plugin) {
+        super(plugin, "EnhancedAgility");
+        setDescription("Magically enhances the user's agility, boosting jump height by 40% and speed by 20% for $1 seconds.");
+        setIdentifiers("skill enhancedagility");
+    }
+
+    @Override
+    public SkillResult use(Hero hero, String[] args) {
+        ArrayList<PotionEffect> toApply = new ArrayList<PotionEffect>(2);
+        toApply.add(0, PotionEffectType.JUMP.createEffect((int) (hero.getLevel()*0.5*20), 2));
+        toApply.add(1, PotionEffectType.SPEED.createEffect((int) (hero.getLevel()*0.5*20), 1));
+        ToolHandlerPlugin.instance.getPotionEffectHandler().addPotionEffectStacking(toApply, hero.getEntity());
+        return SkillResult.NORMAL;
+    }
+
+    @Override
+    public String getDescription(Hero hero) {
+        DecimalFormat dF = new DecimalFormat("##.#");
+        return getDescription().replace("$1", dF.format(hero.getLevel()*0.5) + "");
+    }
+
+}
