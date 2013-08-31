27cc6a6eaa8889aac985328f5f64f99ea80f4a1d
Andrew2060 <2060andrew@gmail.com>
2013-08-31 12:43:07 -0700

    EnhancedAgility: update description to reflect speed removal

--- SkillEnhancedAgility/net/kingdomsofarden/andrew2060/heroes/skills/SkillEnhancedAgility.java
+++ SkillEnhancedAgility/net/kingdomsofarden/andrew2060/heroes/skills/SkillEnhancedAgility.java
@@ -1,9 +1,6 @@
 package net.kingdomsofarden.andrew2060.heroes.skills;
 
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-
-import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import net.kingdomsofarden.andrew2060.toolhandler.ToolHandlerPlugin;
@@ -17,7 +14,7 @@ public class SkillEnhancedAgility extends ActiveSkill {
 
     public SkillEnhancedAgility(Heroes plugin) {
         super(plugin, "EnhancedAgility");
-        setDescription("Magically enhances the user's agility, boosting jump height by 40% and speed by 20% for $1 seconds.");
+        setDescription("Magically enhances the user's agility, boosting jump height by 40% for $1 seconds.");
         setIdentifiers("skill enhancedagility");
     }
 
