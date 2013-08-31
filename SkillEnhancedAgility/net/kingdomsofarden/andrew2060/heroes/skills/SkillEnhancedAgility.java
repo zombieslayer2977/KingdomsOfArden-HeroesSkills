f28a2c74c13145cabee8ee549f40dd22fa0950ae
Andrew2060 <2060andrew@gmail.com>
2013-08-31 12:48:49 -0700

    EnhancedAgility: disable fall damage while active

--- SkillEnhancedAgility/net/kingdomsofarden/andrew2060/heroes/skills/SkillEnhancedAgility.java
+++ SkillEnhancedAgility/net/kingdomsofarden/andrew2060/heroes/skills/SkillEnhancedAgility.java
@@ -1,26 +1,34 @@
 package net.kingdomsofarden.andrew2060.heroes.skills;
 
 import java.text.DecimalFormat;
+
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.potion.PotionEffectType;
 
 import net.kingdomsofarden.andrew2060.toolhandler.ToolHandlerPlugin;
 
 import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.api.events.CharacterDamageEvent;
 import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.effects.ExpirableEffect;
 import com.herocraftonline.heroes.characters.skill.ActiveSkill;
 
-public class SkillEnhancedAgility extends ActiveSkill {
+public class SkillEnhancedAgility extends ActiveSkill implements Listener {
 
     public SkillEnhancedAgility(Heroes plugin) {
         super(plugin, "EnhancedAgility");
         setDescription("Magically enhances the user's agility, boosting jump height by 40% for $1 seconds.");
         setIdentifiers("skill enhancedagility");
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
     @Override
     public SkillResult use(Hero hero, String[] args) {
         ToolHandlerPlugin.instance.getPotionEffectHandler().addPotionEffectStacking(PotionEffectType.JUMP.createEffect((int) (hero.getLevel()*0.5*20), 2), hero.getEntity(), false);
+        hero.addEffect(new ExpirableEffect(this,plugin,"EnhancedAgility",(long) (hero.getLevel() * 0.5 * 1000)));
         return SkillResult.NORMAL;
     }
 
@@ -28,6 +36,15 @@ public class SkillEnhancedAgility extends ActiveSkill {
     public String getDescription(Hero hero) {
         DecimalFormat dF = new DecimalFormat("##.#");
         return getDescription().replace("$1", dF.format(hero.getLevel()*0.5) + "");
+    }
+    
+    @EventHandler(ignoreCancelled = true)
+    public void onCharacterDamage(CharacterDamageEvent event) {
+        if(event.getEntity() instanceof LivingEntity) {
+            if(plugin.getCharacterManager().getCharacter((LivingEntity)event.getEntity()).hasEffect("EnhancedAgility")) {
+                event.setCancelled(true);
+            }
+        }
     }
 
 }
