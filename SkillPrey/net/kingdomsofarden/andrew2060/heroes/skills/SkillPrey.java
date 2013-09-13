5d2cd4280545dd3eed53901f7385c1ac2dde6abf
Andrew2060 <2060andrew@gmail.com>
2013-09-12 18:31:22 -0700

    Prey: Begin work on summoned minion targetting

---
+++ SkillPrey/net/kingdomsofarden/andrew2060/heroes/skills/SkillPrey.java
@@ -0,0 +1,30 @@
+package net.kingdomsofarden.andrew2060.heroes.skills;
+
+import org.bukkit.entity.LivingEntity;
+
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.skill.TargettedSkill;
+
+public class SkillPrey extends TargettedSkill {
+
+    public SkillPrey(Heroes plugin) {
+        super(plugin, "Prey");
+        setDescription("Sets the target for summoned undead minions to attack.");
+    }
+
+    @Override
+    public SkillResult use(Hero hero, LivingEntity target, String[] args) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getDescription(Hero hero) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+
+}
