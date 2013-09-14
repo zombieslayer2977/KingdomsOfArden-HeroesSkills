1020bc2f244917858e04bca988b96fc725779fab
Andrew2060 <2060andrew@gmail.com>
2013-09-14 16:40:45 -0700

    Necromancy: do target checks and actually set a target

--- SkillNecromancy/net/kingdomsofarden/andrew2060/heroes/skills/nms/PathfinderGoalNecromancyTarget.java
+++ SkillNecromancy/net/kingdomsofarden/andrew2060/heroes/skills/nms/PathfinderGoalNecromancyTarget.java
@@ -1,35 +1,52 @@
 package net.kingdomsofarden.andrew2060.heroes.skills.nms;
 
 import org.bukkit.Location;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.Creature;
+import org.bukkit.entity.LivingEntity;
 
+import com.herocraftonline.heroes.characters.Hero;
+
+import net.kingdomsofarden.andrew2060.heroes.skills.api.necromancy.NecromancyTargetManager;
+import net.minecraft.server.v1_6_R2.DamageSource;
 import net.minecraft.server.v1_6_R2.EntityCreature;
 import net.minecraft.server.v1_6_R2.PathfinderGoalTarget;
 
 public class PathfinderGoalNecromancyTarget extends PathfinderGoalTarget {
     
-    EntityCreature entity;
-    private Player summoner;
+    private Hero summoner;
+    private LivingEntity target;
     
-    public PathfinderGoalNecromancyTarget(EntityCreature entity, Player summoner) {
+    public PathfinderGoalNecromancyTarget(EntityCreature entity, Hero summoner) {
         super(entity,false);
-        this.entity = entity;
         this.summoner = summoner;
+        this.target = null;
     }
 
     @Override
     public boolean a() {
         boolean flag = false;
-        if(entity.getGoalTarget() != null) {
-            if(!entity.isAlive()) {
-                return true;    //If target is dead skip subsequent checks
+        if(this.c.getGoalTarget() != null) {
+            if(!c.isAlive()) {
+                return false;    //If target is dead skip subsequent checks
             }
-            Location targetLoc = entity.getGoalTarget().getBukkitEntity().getLocation();
-            if(entity.getBukkitEntity().getLocation().distanceSquared(targetLoc) > 1024) {
+            Location targetLoc = c.getGoalTarget().getBukkitEntity().getLocation();
+            if(this.c.getBukkitEntity().getLocation().distanceSquared(targetLoc) > 1024) {
                 flag = true;
             }
-            if(summoner.getLocation().distanceSquared(targetLoc) > 1024) {
+            if(summoner.getPlayer().getLocation().distanceSquared(targetLoc) > 1024) {
                 flag = true;
+            }
+            if(!summoner.hasEffect("NecromancyTargetManager")) {
+                flag = false;
+                this.c.damageEntity(DamageSource.ANVIL, 10000); //Kill the entity
+            } else {
+                NecromancyTargetManager targetMan = (NecromancyTargetManager) summoner.getEffect("NecromancyTargetManager");
+                target = targetMan.getTarget((Creature) c.getBukkitEntity());
+                if(this.target == null) {
+                    flag = false;
+                } else {
+                    flag = true;
+                }
             }
         } else {
             flag = true;
@@ -37,4 +54,10 @@ public class PathfinderGoalNecromancyTarget extends PathfinderGoalTarget {
         return flag;
     }
 
+    @Override
+    public void c() {
+        ((Creature)this.c.getBukkitEntity()).setTarget(target);
+        super.c();
+    }
+    
 }
