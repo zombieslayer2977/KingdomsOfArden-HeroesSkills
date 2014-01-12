9a8790d6a063b0bccdd764ddb875e6bb569924d0
Andrew2060 <2060andrew@gmail.com>
2014-01-12 14:24:04 -0800

    EarthenArmor: Fix Damage and Damage Reduction

--- SkillEarthenArmor/net/kingdomsofarden/andrew2060/heroes/skills/SkillEarthenArmor.java
+++ SkillEarthenArmor/net/kingdomsofarden/andrew2060/heroes/skills/SkillEarthenArmor.java
@@ -3,8 +3,8 @@ package net.kingdomsofarden.andrew2060.heroes.skills;
 import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.api.SkillResult;
 import com.herocraftonline.heroes.api.events.WeaponDamageEvent;
+import com.herocraftonline.heroes.characters.CharacterTemplate;
 import com.herocraftonline.heroes.characters.Hero;
-import com.herocraftonline.heroes.characters.Monster;
 import com.herocraftonline.heroes.characters.effects.EffectType;
 import com.herocraftonline.heroes.characters.effects.ExpirableEffect;
 import com.herocraftonline.heroes.characters.skill.ActiveSkill;
@@ -12,7 +12,10 @@ import com.herocraftonline.heroes.characters.skill.Skill;
 import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
 import com.herocraftonline.heroes.characters.skill.SkillType;
 import com.herocraftonline.heroes.characters.skill.SkillSetting;
+
+import java.text.DecimalFormat;
 import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
@@ -28,7 +31,7 @@ public class SkillEarthenArmor extends ActiveSkill {
 
     public SkillEarthenArmor(Heroes plugin) {
         super(plugin, "EarthenArmor");
-        setDescription("Passive: Reduces all incoming damage by $1% Active: Shatters the armor, dealing $2 damage to all targets in an area. The armor takes $3 seconds to reaccumulate.");
+        setDescription("Passive: Reduces all incoming damage by $1% Active: Shatters the armor, dealing $2% of current health damage to all targets in an area. The armor takes $3 seconds to reaccumulate.");
         setUsage("/skill earthenarmor");
         setArgumentRange(0, 0);
         setIdentifiers(new String[] { "skill earthenarmor" });
@@ -43,46 +46,38 @@ public class SkillEarthenArmor extends ActiveSkill {
         return section;
     }
 
-    public void init() {
-        super.init();
-        this.applyText = SkillConfigManager.getRaw(this, SkillSetting.APPLY_TEXT, "%hero% shattered their earthen armor!").replace("%hero%", "$1");
-        this.expireText = SkillConfigManager.getRaw(this, SkillSetting.EXPIRE_TEXT, "%hero% has reaccumulated their earthen armor!").replace("%hero%", "$1");
-    }
-    public SkillResult use(Hero hero, String[] args) {
-        if (hero.hasEffect("EAEffect")) {
-            hero.getPlayer().sendMessage("Your Earthen Armor hasn't re-accumulated yet!");
+    @Override
+    public SkillResult use(Hero h, String[] args) {
+        if (h.hasEffect("EAEffect")) {
+            h.getPlayer().sendMessage("Your Earthen Armor hasn't re-accumulated yet!");
             return SkillResult.NORMAL;
         }
-        broadcastExecuteText(hero);
-        int duration = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DURATION, 300000, false);
-        List<Entity> entitylist = hero.getEntity().getNearbyEntities(10.0D, 10.0D, 10.0D);
-        Player p = hero.getPlayer();
-        double damage = p.getHealth() * 2;
+        int duration = SkillConfigManager.getUseSetting(h, this, SkillSetting.DURATION, 300000, false);
+        List<Entity> entitylist = h.getEntity().getNearbyEntities(10.0D, 10.0D, 10.0D);
+        Player p = h.getPlayer();
+        int level = h.getLevel() + h.getHeroClass().getTier() * 75;
+        double dmgMult = 150/(150 + level);
+        double damage = h.getPlayer().getHealth() * dmgMult;
         for (Entity entity : entitylist) {
-            if (!(entity instanceof Player)) {
-                if (((entity instanceof LivingEntity)) && (!(entity instanceof Player))) {
-                    Monster m = this.plugin.getCharacterManager().getMonster((LivingEntity)entity);
-                    Skill.damageCheck(p, m.getEntity());
-                    addSpellTarget(m.getEntity(),hero);
-                    damageEntity(m.getEntity(), p, damage);
-                } else if (((entity instanceof LivingEntity)) && ((entity instanceof Player))) {
-                    Hero m = this.plugin.getCharacterManager().getHero((Player)entity);
-                    Skill.damageCheck(p, m.getEntity());
-                    addSpellTarget(m.getEntity(),hero);
-                    damageEntity(m.getEntity(), p, damage);
-                }
+            if (entity instanceof LivingEntity) {
+                CharacterTemplate cT = this.plugin.getCharacterManager().getCharacter((LivingEntity)entity);
+                Skill.damageCheck(p, cT.getEntity());
+                addSpellTarget(cT.getEntity(),h);
+                damageEntity(cT.getEntity(), p, damage);
             }
         }
 
-        hero.addEffect(new EAEffect(this, duration));
+        h.addEffect(new EAEffect(this, duration));
         return SkillResult.NORMAL;
     }
 
     public String getDescription(Hero hero) {
         int duration = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DURATION, 300000, false);
         double reduct = hero.getLevel() * 0.5D;
-        int dmg = hero.getLevel() * 2;
-        return getDescription().replace("$1", reduct+"").replace("$2", dmg+"").replace("$3", duration*0.001+"");
+        int level = hero.getLevel() + hero.getHeroClass().getTier() * 75;
+        double dmg = 150/(150 + level);
+        DecimalFormat dF = new DecimalFormat("##.##");
+        return getDescription().replace("$1", reduct+"").replace("$2", dF.format(dmg)).replace("$3", duration*0.001+"");
     }
     public class EAEffect extends ExpirableEffect {
         public EAEffect(Skill skill, long duration) {
@@ -103,23 +98,22 @@ public class SkillEarthenArmor extends ActiveSkill {
     public class EAListener implements Listener {
         public EAListener() {}
 
-        @EventHandler(priority=EventPriority.HIGHEST)
-        public void onWeaponDamage(WeaponDamageEvent event) { if (!event.isCancelled()) {
-            return;
-        }
-        if (!(event.getDamager() instanceof Player)) {
-            return;
-        }
-        Player p = (Player)event.getDamager();
-        Hero h = SkillEarthenArmor.this.plugin.getCharacterManager().getHero(p);
-        if (!h.hasAccessToSkill("EarthenArmor")) {
-            return;
-        }
-        if (h.hasEffect("EAEffect")) {
-            return;
-        }
-        double levelMultiplier = (100.0D - h.getLevel() * 0.5D) * 0.01D;
-        event.setDamage(event.getDamage() * levelMultiplier);
+        @EventHandler(priority=EventPriority.HIGHEST, ignoreCancelled = true)
+        public void onWeaponDamage(WeaponDamageEvent event) { 
+            if (!(event.getDamager() instanceof Player)) {
+                return;
+            }
+            Player p = (Player)event.getDamager();
+            Hero h = SkillEarthenArmor.this.plugin.getCharacterManager().getHero(p);
+            if (!h.hasAccessToSkill("EarthenArmor")) {
+                return;
+            }
+            if (h.hasEffect("EAEffect")) {
+                return;
+            }
+            int level = h.getLevel() + h.getHeroClass().getTier() * 75;
+            double levelMultiplier = 150/(150 + level);
+            event.setDamage(event.getDamage() * levelMultiplier);
         }
     }
 }
